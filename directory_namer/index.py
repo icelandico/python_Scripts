b84e12db269e0e62ff0b6eee1535bbfbb32cf2c6
Michal Muszynski <m.muszy@gmail.com>
2021-09-14 20:10:10 +0200

    Refactor script, comment wrong function

--- directory_namer/index.py
+++ directory_namer/index.py
@@ -3,17 +3,16 @@ import os
 entries = os.listdir('./example')
 file_path = os.path.abspath('./example')
 
+# Removes the files from directory and moves to one folder up
 # for index, name in enumerate(entries):
 #     current_name = name
 #     new_name = f"File_nr_{index}"
 #     if name.path.endswith('.txt'):
 #         os.rename(current_name, new_name)
-print(file_path)
+
+# Fixed version, renames the files in place
 for index, name in enumerate(entries):
     current_name = name
     _, ext = os.path.splitext(name)
     if name.endswith('.txt'):
         os.rename(os.path.join(file_path, name), os.path.join(file_path, str(index) + ext))
-    # new_name = f"File_nr_{index}"
-    # if name.path.endswith('.txt'):
-    #     print(os.chdir('./example'))
